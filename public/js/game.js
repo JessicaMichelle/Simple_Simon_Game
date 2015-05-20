a1bf8f90e2ef5db7e6cc440121346c2ad1862a11
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-05-19 21:28:54 -0500

    Initial Commit - new files for Simple Simon game. Set layout out. Added for the buttons to light up when clicked and then return back to their previous opacity.

---
+++ public/js/game.js
@@ -0,0 +1,24 @@
+"use strict";
+
+$(document).ready(function() {
+	$(".box1").click(function () {
+		$(this)
+			.animate({opacity: "1"}, 1)
+			.animate({opacity: ".50"}, 500)
+	});
+	$(".box2").click(function () {
+		$(this)
+			.animate({opacity: "1"}, 1)
+			.animate({opacity: ".50"}, 500)
+	});
+	$(".box3").click(function () {
+		$(this)
+			.animate({opacity: "1"}, 1)
+			.animate({opacity: ".50"}, 500)
+	});
+	$(".box4").click(function () {
+		$(this)
+			.animate({opacity: "1"}, 1)
+			.animate({opacity: ".50"}, 500)
+	});
+})
