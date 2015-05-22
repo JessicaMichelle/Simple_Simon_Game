7bd280dbe4f690839265983bb987e5ec25a5c758
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-05-21 21:26:57 -0500

    Checks to see if two arrays match. Round adds. Simon button activates. Need to still make it sequence through the array for simon.

--- public/js/game.js
+++ public/js/game.js
@@ -1,24 +1,65 @@
 "use strict";
 
 $(document).ready(function() {
-	$(".box1").click(function () {
-		$(this)
-			.animate({opacity: "1"}, 1)
-			.animate({opacity: ".50"}, 500)
-	});
-	$(".box2").click(function () {
-		$(this)
-			.animate({opacity: "1"}, 1)
-			.animate({opacity: ".50"}, 500)
-	});
-	$(".box3").click(function () {
-		$(this)
-			.animate({opacity: "1"}, 1)
-			.animate({opacity: ".50"}, 500)
-	});
-	$(".box4").click(function () {
-		$(this)
-			.animate({opacity: "1"}, 1)
-			.animate({opacity: ".50"}, 500)
+	var simon = [];
+	var player = [];
+	var divColor = $(".container").children();
+	var timer = 5;
+	var round = 1;
+	var timeout;
+	var interval;
+	var i = 0;
+	
+	function simonChoice() {
+		var random = Math.floor(Math.random()*(divColor.length-1));
+		// var selectedDiv = $(divColor[random]);
+		timeout = setInterval(function () {
+		simon.push(random);
+			if(i < simon.length) {
+				$(divColor[simon[i]])
+					.animate({opacity: "1"}, 100)
+					.animate({opacity: ".50"}, 1000);
+				console.log("simon array = " + simon);
+				i++;
+			} else {
+				clearInterval(timeout);
+			}
+		}, 500);
+		interval = setInterval(updateTimer, 1000);
+	}
+
+	function updateTimer() {
+		if (timer == 0) {
+			alert("Game Over. Simon Wins.");
+		} else if (timer > 0) {
+			$(".start").html(timer);
+		};
+		timer--;
+	};
+
+	$(".start").click(function () {
+		$("h2").html("Watch The Sequence")
+		simonChoice();
+		$('.box').click(function () {
+			clearInterval(interval);
+			player.push($(this).data("number"));
+			timer = 5;
+			$(".start").html(timer);
+			$(this)
+				.animate({opacity: "1"}, 100)
+				.animate({opacity: ".50"}, 500);
+			simonChoice();	
+			console.log("player array = " + player);
+			if (simon.length == player.length) {
+				if (simon.toString() == player.toString()) {
+					round++;
+					$("#round").html(round);
+					console.log("test");
+				} else if (simon.toString() != player.toString()) {
+					clearInterval(interval);
+					alert("Game Over. Simon Wins.");
+				}
+			};
+		});
 	});
 })
